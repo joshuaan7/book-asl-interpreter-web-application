d1ebbfab9a15494382b7a4bdca658910c2027a68
Joshua An <joshuaan7@gmail.com>
2021-10-17 15:26:41 -0400

    files added, finished video 3

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const app = express();
+
+app.use(bodyParser.json());
+
+
+app.use('/graphql', graphqlHTTP({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => {
+      return ['Romantic Cooking', 'Sailing', 'All-night coding']
+    },
+    createEvent: (args) => {
+      const eventName = args.name;
+      return eventName;
+    },
+    graphiql: true
+  }
+}));
+
+app.get('/', (req,res,next) => {
+  res.send('Hello World!');
+})
+
+app.listen(3000);
